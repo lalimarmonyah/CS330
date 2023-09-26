95b8eab9b7d0b8aa176516a1bfa831c2cf58cd55
lalimarmonyah <144646068+lalimarmonyah@users.noreply.github.com>
2023-09-26 12:16:44 -0400

    Add files via upload

---
+++ varNames.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    // int
+    int num = 15;
+    // double - can store 15 decimal digits
+    double doubleNum = 10.99;
+    // float - can strore 6-7 decimal digits
+    float floatNum = 9.88;
+    // char - use single quotes
+    char letter = 'F';
+    // string - use double quotes
+    string name = "Nyah";
+    // bool
+    bool boolean = true;
+
+    cout << "My name is " << name << ".";
+
+    
+}
+
+// operators: +,-,*,/,%,++,--
+// ++var or --var
+
+// assignment = and += -=, other operators even logical operators
+
+// pointer variables
+// reference variables
+// static variables
